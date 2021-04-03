2384777f8909f9a7faff1c34f1fbc6ad605868b6
Davide <davidesavarro98@gmail.com>
2021-04-03 23:32:03 +0200

    Completed deserialization and definition of equality

--- src/main.py
+++ src/main.py
@@ -7,8 +7,20 @@ import json
 def main():
     scraper = AmazonScraper(None)
     foundItems = scraper.searchItems("ssd", debug=False)
-    for item in foundItems:
-        print(json.dumps(item, indent=4, cls=AmazonItemEncoder))
+
+    fp = open("testData.json", "w")
+    fp.write(json.dumps(foundItems, indent=4, cls=AmazonItemEncoder))
+    fp.close()
+
+    fp = open("testData.json", "r")
+    dic = json.load(fp)
+
+    for i in range(5):
+        print("Printing object " + str(i+1))
+        print(foundItems[i])
+        print(AmazonItem.jsonToObject(jsonDict=dic[i]))
+        print(foundItems[i] == AmazonItem.jsonToObject(jsonDict=dic[i]))
+        print("*************************************")
 
 
 if __name__ == "__main__":
--- src/model/amazonItem.py
+++ src/model/amazonItem.py
@@ -2,6 +2,7 @@
 import datetime
 from json import JSONEncoder
 from varname import nameof
+from price_parser import Price
 
 class AmazonItem:
 
@@ -17,13 +18,39 @@ class AmazonItem:
         self.ueDate = ueDate
 
     @staticmethod
-    def jsonToObject(self, jsonDict):
-        # TODO Converts json string to object
-        return AmazonItem()
+    def jsonToObject(jsonDict):
+        # Converts json string to object
+        price = None if jsonDict[AmazonItemKeys.priceAmount] == None or jsonDict[AmazonItemKeys.priceCurrency] == None else Price.fromstring(str(jsonDict[AmazonItemKeys.priceAmount]) + " " + jsonDict[AmazonItemKeys.priceCurrency]) 
+        usedPrice = None if jsonDict[AmazonItemKeys.usedPriceAmount] == None or jsonDict[AmazonItemKeys.usedPriceCurrency] == None else Price.fromstring(str(jsonDict[AmazonItemKeys.usedPriceAmount]) + " " + jsonDict[AmazonItemKeys.usedPriceCurrency])
+        return AmazonItem(jsonDict[AmazonItemKeys.id], jsonDict[AmazonItemKeys.title], jsonDict[AmazonItemKeys.stars], jsonDict[AmazonItemKeys.objLink], 
+                            jsonDict[AmazonItemKeys.imgLink], price, usedPrice, jsonDict[AmazonItemKeys.isPrime], 
+                            datetime.datetime.fromisoformat(jsonDict[AmazonItemKeys.ueDate]))
     
     def __str__(self):
-        return AmazonItemKeys.id + " " + self.id + "\n" + AmazonItemKeys.imgLink + " " + self.imgLink + "\n" + AmazonItemKeys.objLink + " " + self.objLink + "\n" + AmazonItemKeys.title + " " + self.title + "\n" + AmazonItemKeys.stars + " " + str(self.stars) + "\n" + AmazonItemKeys.price + " " + str(self.price) + "\n" + AmazonItemKeys.usedPrice + " " + str(self.usedPrice) + "\n" + AmazonItemKeys.isPrime + " " + str(self.isPrime) + "\n"
+        return AmazonItemKeys.id + ": " + self.id + "\n" + AmazonItemKeys.imgLink + ": " + self.imgLink + "\n" + AmazonItemKeys.objLink + ": " + self.objLink + "\n" + AmazonItemKeys.title + ": " + self.title + "\n" + AmazonItemKeys.stars + ": " + str(self.stars) + "\n" + AmazonItemKeys.price + ": " + str(self.price) + "\n" + AmazonItemKeys.usedPrice + ": " + str(self.usedPrice) + "\n" + AmazonItemKeys.isPrime + ": " + str(self.isPrime) + AmazonItemKeys.ueDate + ": " + self.ueDate.isoformat() + "\n"
     
+    def __eq__(self, other):
+        if isinstance(other, AmazonItem):
+            isPriceEqual = self.isPriceEq(self.price, other.price)
+            isUsedPriceEqual = self.isPriceEq(self.usedPrice, other.usedPrice)
+            return self.id == other.id and self.title == other.title and self.stars == other.stars and self.objLink == other.objLink and self.imgLink == other.imgLink and isPriceEqual and isUsedPriceEqual and self.isPrime == other.isPrime and self.ueDate.isoformat() == other.ueDate.isoformat()
+        return False
+    
+    def isPriceEq(self, thisPrice, thatPrice):
+        if(thisPrice is not None and thatPrice is not None):
+            return thisPrice.amount_float == thatPrice.amount_float and thisPrice.currency == thatPrice.currency
+        else:
+            if(thisPrice is None and thatPrice is None): return True
+            else: return False
+
+    @staticmethod
+    def isPriceEqStatic(thisPrice, thatPrice):
+        if(thisPrice is not None and thatPrice is not None):
+            return thisPrice.amount_float == thatPrice.amount_float and thisPrice.currency == thatPrice.currency
+        else:
+            if(thisPrice is None and thatPrice is None): return True
+            else: return False    
+
 class AmazonItemEncoder(JSONEncoder):
 
     def default(self, o):
@@ -47,8 +74,10 @@ class AmazonItemKeys:
     stars = "stars"
     objLink = "objLink"
     imgLink = "imgLink"
+    price = "price"
     priceAmount = "priceAmount"
     priceCurrency = "priceCurrency"
+    usedPrice = "usedPrice"
     usedPriceAmount = "usedPriceAmount"
     usedPriceCurrency = "usedPriceCurrency"
     isPrime = "isPrime"
