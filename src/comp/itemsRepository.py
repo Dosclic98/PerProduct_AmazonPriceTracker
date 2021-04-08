d9edb425ef889ea69ce4ff80d76babf0eccd95f9
Davide <davidesavarro98@gmail.com>
2021-04-09 00:48:15 +0200

    Moved items storage to a specific folder and fixed bug where it wont load empty storage file

--- src/comp/itemsRepository.py
+++ src/comp/itemsRepository.py
@@ -6,18 +6,19 @@ from src.model.amazonItem import AmazonItem, AmazonItemEncoder
 
 class ItemRepo:
 
+    storagePath = "./data/storage/"
+
     def __init__(self, repoFileName):
         self.repoFileName = repoFileName
 
     def load(self):
-        try:
-            fp = open(self.repoFileName, "r")
-        except FileNotFoundError:
-            return set()
-
-        dic = json.load(fp)
-
         savedItems = set()
+        try:
+            fp = open(self.storagePath + self.repoFileName + ".json", "r")
+            dic = json.load(fp)
+        except (FileNotFoundError, ValueError):
+            return savedItems
+
         for item in dic:
             savedItems.add(AmazonItem.jsonToObject(item))
 
