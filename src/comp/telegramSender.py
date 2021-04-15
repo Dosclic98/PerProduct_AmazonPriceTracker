98c598edc802491db3f1cc90e1f439f8e8282b9f
Davide <davidesavarro98@gmail.com>
2021-04-16 00:11:14 +0200

    Implemented method for lower price (used or not) notification

--- src/comp/telegramSender.py
+++ src/comp/telegramSender.py
@@ -14,7 +14,9 @@ class TeleBotSender:
     maxSendFail = 5
 
     def __init__(self, chatIdKey):
-        dic = json.load(open(self.botInfoPath + "botApiKey.json"))
+        fp = open(self.botInfoPath + "botApiKey.json")
+        dic = json.load(fp)
+        fp.close()
         self.key = dic["key"]
         self.chat_id = dic[chatIdKey]
         self.bot = Bot(self.key)
@@ -47,20 +49,54 @@ class MessageBuilder:
     
     starEmoji = "\U00002b50"
     redDotEmoji = "\U0001f534"
+    greenDotEmoji = "\U0001f7e2"
     moneyEmoji = "\U0001f4b0"
+    flyMoneyEmoji = "\U0001f4b8"
     deliveryEmoji = "\U0001f69a"
 
     @staticmethod
     def newItemFound(obj):
-        baseStr = MessageBuilder.redDotEmoji + " <b>Nuovo prodotto trovato!</b>\n\n"
+        baseStr = MessageBuilder.greenDotEmoji + " <b>Nuovo prodotto!</b>\n\n"
         if(obj.title != None):
             baseStr += "<b>" + obj.title + "</b>" + "\n\n"
-        priceStr = "\n" + MessageBuilder.moneyEmoji + "  Prezzo nuovo:  {}"
+        priceStr = "\n" + (MessageBuilder.flyMoneyEmoji if obj.discount != None else MessageBuilder.moneyEmoji) + " Prezzo nuovo:  {}"
+        discStr = " con uno sconto del {}%"
         usedPriceStr = "\n" + MessageBuilder.moneyEmoji + " Prezzo usato:   {}"
         if(obj.price != None and obj.price.amount_float != None and obj.price.currency != None):
-            baseStr += priceStr.format(str(obj.price.amount_float) + " " + str(obj.price.currency))
+            if obj.discount != None:
+                baseStr += (priceStr.format(str("%.2f" % (obj.price.amount_float - (obj.price.amount_float * (obj.discount/100)))) + " " + str(obj.price.currency)) + discStr.format(str(obj.discount)))
+            else:
+                baseStr += priceStr.format(str("%.2f" % obj.price.amount_float) + " " + str(obj.price.currency))
         if(obj.usedPrice != None and obj.usedPrice.amount_float != None and obj.usedPrice.currency != None):
-            baseStr += usedPriceStr.format(str(obj.usedPrice.amount_float) + " " + str(obj.usedPrice.currency))
+            baseStr += usedPriceStr.format(str("%.2f" % obj.usedPrice.amount_float) + " " + str(obj.usedPrice.currency))
+
+        starsStr = "\n" + MessageBuilder.starEmoji + "  {}"+ " / 5"
+        if(obj.stars != None):
+            baseStr += starsStr.format(str(obj.stars))
+        
+        if(obj.isPrime):
+            baseStr += "\n" + MessageBuilder.deliveryEmoji + "  Spedizione Prime"
+
+        if(obj.imgLink != None):
+            baseStr += '<a href="'+ obj.imgLink +'">&#8205;</a>'
+        
+        return (baseStr, obj.objLink)
+
+    @staticmethod
+    def newDiscountFound(obj):
+        baseStr = MessageBuilder.redDotEmoji + " <b>Nuovo sconto!</b>\n\n"
+        if(obj.title != None):
+            baseStr += "<b>" + obj.title + "</b>" + "\n\n"
+        priceStr = "\n" + (MessageBuilder.flyMoneyEmoji if obj.discount != None else MessageBuilder.moneyEmoji) + " Prezzo nuovo:  {}"
+        discStr = " con uno sconto del {}%"
+        usedPriceStr = "\n" + MessageBuilder.moneyEmoji + " Prezzo usato:   {}"
+        if(obj.price != None and obj.price.amount_float != None and obj.price.currency != None):
+            if obj.discount != None:
+                baseStr += (priceStr.format(str("%.2f" % (obj.price.amount_float - (obj.price.amount_float * (obj.discount/100)))) + " " + str(obj.price.currency)) + discStr.format(str(obj.discount)))
+            else:
+                baseStr += priceStr.format(str("%.2f" % obj.price.amount_float) + " " + str(obj.price.currency))
+        if(obj.usedPrice != None and obj.usedPrice.amount_float != None and obj.usedPrice.currency != None):
+            baseStr += usedPriceStr.format(str("%.2f" % obj.usedPrice.amount_float) + " " + str(obj.usedPrice.currency))
 
         starsStr = "\n" + MessageBuilder.starEmoji + "  {}"+ " / 5"
         if(obj.stars != None):
@@ -72,6 +108,49 @@ class MessageBuilder:
         if(obj.imgLink != None):
             baseStr += '<a href="'+ obj.imgLink +'">&#8205;</a>'
 
-
-        
         return (baseStr, obj.objLink)
+
+    @staticmethod
+    def betterPriceFound(stored, obj):
+        baseStr = MessageBuilder.redDotEmoji + " <b>Calo di prezzo!</b>\n\n"
+        if(obj.title != None):
+            baseStr += "<b>" + obj.title + "</b>" + "\n\n"
+        priceStr = "\n" + (MessageBuilder.flyMoneyEmoji if obj.discount != None else MessageBuilder.moneyEmoji) + " Prezzo nuovo:  {}"
+        discStr = " con uno sconto del {}%"
+        usedPriceStr = "\n" + MessageBuilder.moneyEmoji + " Prezzo usato:   {}"
+        if(stored.price != None and stored.price.amount_float != None and stored.price.currency != None and obj.price != None and obj.price.amount_float != None and obj.price.currency != None):
+            if obj.discount != None:
+                if stored.price.amount_float > obj.price.amount_float:
+                    baseStr += (priceStr.format(str("%.2f" % (obj.price.amount_float - (obj.price.amount_float * (obj.discount/100)))) + " " + str(obj.price.currency)) + discStr.format(str(obj.discount)) + " invece di " + str("%.2f" % stored.price.amount_float) + " " + str(stored.price.currency))
+                else:
+                    baseStr += (priceStr.format(str("%.2f" % (obj.price.amount_float - (obj.price.amount_float * (obj.discount/100)))) + " " + str(obj.price.currency)) + discStr.format(str(obj.discount)))
+            else:
+                if stored.price.amount_float > obj.price.amount_float:
+                    baseStr += priceStr.format(str("%.2f" % obj.price.amount_float) + " " + str(obj.price.currency) + " invece di " + str("%.2f" % stored.price.amount_float) + " " + str(stored.price.currency))
+                else:
+                    baseStr += priceStr.format(str("%.2f" % obj.price.amount_float) + " " + str(obj.price.currency))
+        elif(stored.price == None and obj.price != None and obj.price.amount_float != None and obj.price.currency != None):
+            if obj.discount != None:
+                baseStr += (priceStr.format(str("%.2f" % (obj.price.amount_float - (obj.price.amount_float * (obj.discount/100)))) + " " + str(obj.price.currency)) + " appena aggiunto" + discStr.format(str(obj.discount)))
+            else:
+                baseStr += priceStr.format(str("%.2f" % obj.price.amount_float) + " " + str(obj.price.currency) + " appena aggiunto!")
+        # If the used price has gotten better or a new used element has been added
+        if(stored.usedPrice != None and stored.usedPrice.amount_float != None and stored.usedPrice.currency != None and obj.usedPrice != None and obj.usedPrice.amount_float != None and obj.usedPrice.currency != None):
+            if stored.usedPrice.amount_float > obj.usedPrice.amount_float:
+                baseStr += usedPriceStr.format(str("%.2f" % obj.usedPrice.amount_float) + " " + str(obj.usedPrice.currency) + " invece di " + str("%.2f" % stored.usedPrice.amount_float) + " " + str(stored.usedPrice.currency))
+            else:
+                baseStr += usedPriceStr.format(str("%.2f" % obj.usedPrice.amount_float) + " " + str(obj.usedPrice.currency))
+        elif(stored.usedPrice == None and obj.usedPrice != None and obj.usedPrice.amount_float != None and obj.usedPrice.currency != None):
+            baseStr += usedPriceStr.format(str("%.2f" % obj.usedPrice.amount_float) + " " + str(obj.usedPrice.currency) + " appena aggiunto!")
+
+        starsStr = "\n" + MessageBuilder.starEmoji + "  {}"+ " / 5"
+        if(obj.stars != None):
+            baseStr += starsStr.format(str(obj.stars))
+        
+        if(obj.isPrime):
+            baseStr += "\n" + MessageBuilder.deliveryEmoji + "  Spedizione Prime"
+
+        if(obj.imgLink != None):
+            baseStr += '<a href="'+ obj.imgLink +'">&#8205;</a>'
+
+        return (baseStr, obj.objLink)        
--- src/main.py
+++ src/main.py
@@ -9,51 +9,62 @@ from src.comp.telegramSender import TeleBotSender, MessageBuilder
 bot = None
 
 def main():
+    # TODO Introduction of parameter system for command line execution
+    # TODO Add method to initialize repository doing 5 sequential execution of the program (to completely fill the repository) 
     global bot
     bot = TeleBotSender("@OfferteStorage")
 
     scraper = AmazonScraper()
-    foundItemsSet = scraper.searchItemsMultiple(["ssd"])
-    repoFileName = "testData"
+    foundItemsSet = scraper.searchItems("ssd hard disk")
+    repoFileName = "lolData"
     repo = ItemRepo(repoFileName)
     repoItemsSet = repo.load()
     print(len(foundItemsSet))
     itemsToStore = updateRepo(repoItemsSet, foundItemsSet)
-    #repo.save(itemsToStore)
+    repo.save(itemsToStore)
 
 def updateRepo(savedItems, retrievedItems):
     global bot
     setToStore = set()
     savedItemsMap = buildItemsMap(savedItems)
+    print(len(savedItemsMap))
     for item in retrievedItems:
         # 1) Retrieve the item if stored before
+        storedItem = None
         try:
             storedItem = savedItemsMap[item.id]
         except KeyError:
             storedItem = None
+            assert(not savedItems.__contains__(item))
         # 2.a) If no such item is present, then store the retrieved item and notify it through a message
         if(storedItem == None):
             bot.sendMessage(MessageBuilder.newItemFound(item))
-            setToStore.add(item)
         else:
-            if (( ((item.price != None and storedItem.price != None) and (item.price.amount_float < storedItem.price.amount_float)) or 
+            if item.discount != None and ((storedItem.discount == None) or (storedItem.discount != None and item.discount > storedItem.discount)):
+                # Found a new discount
+                bot.sendMessage(MessageBuilder.newDiscountFound(item))
+            elif (( ((item.price != None and storedItem.price != None) and (item.price.amount_float < storedItem.price.amount_float)) or 
                     (item.price != None and storedItem.price == None)) and
                 ( ((item.usedPrice != None and storedItem.usedPrice != None) and (item.usedPrice.amount_float < storedItem.usedPrice.amount_float)) or
                     (item.usedPrice != None and storedItem.usedPrice == None))):
-                # TODO Bot new price and used price have gotten "better"
-                print("Btr")
+                # Bot new price and used price have gotten "better"
+                bot.sendMessage(MessageBuilder.betterPriceFound(stored=storedItem, obj=item))
             elif ( ((item.price != None and storedItem.price != None) and (item.price.amount_float < storedItem.price.amount_float)) or 
                     (item.price != None and storedItem.price == None)):
                 # TODO Only new peice has gotten better
-                print("New Btr")
+                pass
             elif ( ((item.usedPrice != None and storedItem.usedPrice != None) and (item.usedPrice.amount_float < storedItem.usedPrice.amount_float)) or
                     (item.usedPrice != None and storedItem.usedPrice == None)):
                 # TODO only used price has goten better
-                print("Used Btr")
+                pass
             else:
-                # TODO just store the result without having to notify
-                print("Not Btr")
-                
+                # Just store the result without having to notify
+                pass
+        setToStore.add(item)
+    for stItem in savedItems:
+        if not setToStore.__contains__(stItem):
+            setToStore.add(stItem)
+            
     return setToStore
 
 def buildItemsMap(savedItems):
